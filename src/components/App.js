9036d568c106d0e612543c01a635e554360b8286
Gloves Off <114874765+AttackingInternetFascists@users.noreply.github.com>
2023-02-09 07:41:27 +0000

    Mission control

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,67 @@
 import React from 'react'
 
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Card from 'react-bootstrap/Card'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
+
+import Select from 'react-select'
+import Webcam from "react-webcam"
+
+export default () => {
+  const [selectedDevice, setSelectedDevice] = React.useState()
+  const [deviceOptions, setDeviceOptions] = React.useState([])
+
+  React.useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      setDeviceOptions(devices.map(device => ({
+        value: device.deviceId,
+        label: device.label
+      })))
+    })
+  }, [setDeviceOptions])
+
+  const onSelectDevice = React.useCallback(selectedDevice => {
+    setSelectedDevice(selectedDevice)
+  }, [setSelectedDevice])
+
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Mission Control</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container className="my-3">
+        <Row>
+          <Col>
+            <Card>
+              { selectedDevice ? (
+                <Card.Body>
+                  <Webcam className="w-100" audio={false} videoConstraints={{ deviceId: selectedDevice.value }} /> 
+                </Card.Body>
+              ) : null }
+              <Card.Footer>
+                <Select
+                  value={selectedDevice}
+                  options={deviceOptions}
+                  onChange={onSelectDevice}
+                />
+              </Card.Footer>
+            </Card>
+          </Col>
+          
+          <Col>
+            <Card>
+              <Card.Header>Detections</Card.Header>
+              <Card.Body></Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
