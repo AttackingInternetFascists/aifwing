7e3a8de684f29cd7f024c22f56138592595ce67f
Gloves Off <114874765+AttackingInternetFascists@users.noreply.github.com>
2023-02-09 08:13:47 +0000

    Fix gh-pages static assets issue

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const [ subdomain, path ] = process.env.GITHUB_REPOSITORY.split('/')
+
+module.exports = () => {
+  return {
+    webpack: {
+      configure: (webpackConfig, { env }) => {
+        const htmlWebpackPluginInstance = webpackConfig.plugins.find(p => p instanceof HtmlWebpackPlugin)
+        
+        if (htmlWebpackPluginInstance) {
+          // htmlWebpackPluginInstance.userOptions.minify = false
+          htmlWebpackPluginInstance.userOptions.publicPath = `https://${subdomain}.github.io/${path}`
+        }
+
+        return webpackConfig
+      }
+    }
+  }
+}
