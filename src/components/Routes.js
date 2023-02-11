5151cbc0344e25c11d6d43ed7c92287e7e8c1ca4
Gloves Off <114874765+AttackingInternetFascists@users.noreply.github.com>
2023-02-11 01:47:12 +0000

    remove /aifwing prefix for HashRouter

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Layout from './Layout'
 import AccessibleInstallFirmware from './AccessibleInstallFirmware'
@@ -8,13 +8,9 @@ import AutomaticallyIdentifyFascists from './AutomaticallyIdentifyFascists'
 export default () => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<Navigate replace to="/aifwing" />} />
-
-      <Route path="aifwing">
-        <Route index element={<AutomaticallyIdentifyFascists />} />
-        <Route path="flasher" element={<AccessibleInstallFirmware />} />
-        <Route path="*" element={<h3>Every day is STFU Friday.</h3>}></Route>
-      </Route>
+      <Route index element={<AutomaticallyIdentifyFascists />} />
+      <Route path="flasher" element={<AccessibleInstallFirmware />} />
+      <Route path="*" element={<h3>Every day is STFU Friday.</h3>}></Route>
     </Route>    
   </Routes>
 )
